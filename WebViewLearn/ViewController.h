2530b1a8f4f405618ca7cbb1c306e6062480e24a
srcxiaoshi <869259080@qq.com>
2016-08-11 13:32:47 +0800

    Initial Commit

---
+++ WebViewLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewLearn
+//
+//  Created by baidu on 16/8/11.
+//  Copyright © 2016年 shiruichang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebViewLearn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebViewLearn
+//
+//  Created by baidu on 16/8/11.
+//  Copyright © 2016年 shiruichang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
